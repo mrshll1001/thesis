242c099920a4aac1d37519466282560368d2cf6f
Matt Marshall <m.marshall@newcastle.ac.uk>
2018-08-07 14:58:19 +0100

    Updated build script and index, added a custom css file

--- buildhtml.sh
+++ buildhtml.sh
@@ -7,6 +7,9 @@ echo "Building HTML"
 
 
 # Run the Pandoc command
-pandoc src/index.md src/introduction.md src/lit_review.md src/methodoloy.md src/fieldwork_casestudy.md src/designing_accounting_tools.md src/evaluation.md src/discussion.md src/conclusions.md src/bibliography.md -o html/index.html -s --toc --filter=pandoc-citeproc
+pandoc src/index.md src/introduction.md src/lit_review.md src/methodoloy.md src/fieldwork_casestudy.md src/designing_accounting_tools.md src/evaluation.md src/discussion.md src/conclusions.md src/bibliography.md -o html/index.html -s --toc --filter=pandoc-citeproc --self-contained
+
+# Copy css into the html directory
+
 
 echo "HTML copy built under /html"
--- src/index.md
+++ src/index.md
@@ -3,6 +3,10 @@ title: A Rough, Transparent, Draft of my PhD Thesis
 author: Matt Marshall
 bibliography: bibliography.bib
 
+css:
+ - https://unpkg.com/purecss@1.0.0/build/pure-min.css
+ - ./templates/web.css
+
 link-citations: true
 csl: sigchi.csl
 ---
