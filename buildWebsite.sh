d65caa32aed4dac8c34fd02669ed107dd6595590
Matt Marshall <matt@mrshll.uk>
2018-11-13 07:29:16 +0000

    Scripts to build a full site

---
+++ buildWebsite.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# Build the html
+/bin/bash ./buildhtml.sh
+/bin/bash ./buildwp.sh
+
+# Send the html folder up to docker
+echo "Setting up file structure"
+
+mkdir -p ./out/website
+
+cp ./templates/index.html ./out/website
+cp ./templates/web.css ./out/website
+
+cp ./out/html/thesis.html ./out/website
+cp ./out/word-processor/thesis.docx ./out/website
--- buildhtml.sh
+++ buildhtml.sh
@@ -7,6 +7,6 @@ echo "Building HTML"
 
 
 # Run the Pandoc command
-pandoc src/index.md src/introduction.md src/lit_review.md src/methodoloy.md src/fieldwork_casestudy.md src/designing_accounting_tools.md src/evaluation.md src/discussion.md src/conclusions.md src/bibliography.md -o out/html/index.html -s --toc --toc-depth=2 --filter=pandoc-citeproc --self-contained
+pandoc src/index.md src/introduction.md src/lit_review.md src/methodoloy.md src/fieldwork_casestudy.md src/designing_accounting_tools.md src/evaluation.md src/discussion.md src/conclusions.md src/bibliography.md -o out/html/thesis.html -s --toc --toc-depth=2 --filter=pandoc-citeproc --self-contained
 
 echo "HTML copy built under /out/html"
